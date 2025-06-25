1c24b0f052bc61360b94a6178c0d05288d08a83e
almaaprl <162028579+almaaprl@users.noreply.github.com>
2025-06-25 22:34:40 +0700

    Update http.py

--- Tugas 4/http.py
+++ Tugas 4/http.py
@@ -3,6 +3,7 @@ import os.path
 import uuid
 from glob import glob
 from datetime import datetime
+import urllib.parse
 
 class HttpServer:
 	def __init__(self):
@@ -12,6 +13,7 @@ class HttpServer:
 		self.types['.jpg']='image/jpeg'
 		self.types['.txt']='text/plain'
 		self.types['.html']='text/html'
+  
 	def response(self,kode=404,message='Not Found',messagebody=bytes(),headers={}):
 		tanggal = datetime.now().strftime('%c')
 		resp=[]
@@ -37,8 +39,41 @@ class HttpServer:
 		#response adalah bytes
 		return response
 
-	def proses(self,data):
-		
+	def list(self):
+		files = os.listdir('.')
+		file_list = '\n'.join(files)
+		return self.response(200, 'OK', file_list, {'Content-type': 'text/plain'})
+
+	def upload(self, headers, body):
+		filename = None
+		content_length = 0
+		for line in headers:
+			if line.lower().startswith('filename:'):
+				filename = line.split(':', 1)[1].strip()
+			if line.lower().startswith('content-length:'):
+				content_length = int(line.split(':', 1)[1].strip())
+
+		if filename:
+			body_bytes = body.encode()
+			with open(os.path.join('.', filename), 'wb') as f:
+				f.write(body_bytes[:content_length])
+			return self.response(200, 'OK', f'File {filename} berhasil diupload', {'Content-type': 'text/plain'})
+		return self.response(400, 'Bad Request', 'Filename header missing', {})
+
+	def delete(self, object_address):
+		query = urllib.parse.urlparse(object_address).query
+		params = urllib.parse.parse_qs(query)
+		filename = params.get('file', [None])[0]
+		if filename:
+			filepath = os.path.join('.', filename)
+			if os.path.exists(filepath):
+				os.remove(filepath)
+				return self.response(200, 'OK', f'File {filename} berhasil dihapus', {'Content-type': 'text/plain'})
+			else:
+				return self.response(404, 'Bad request', f'{filename} tidak ada', {})
+		return self.response(400, 'Bad Request', 'Filename query missing', {})
+ 
+	def proses(self,data):	
 		requests = data.split("\r\n")
 		#print(requests)
 
@@ -50,16 +85,31 @@ class HttpServer:
 		j = baris.split(" ")
 		try:
 			method=j[0].upper().strip()
+			object_address = j[1].strip()
+   
 			if (method=='GET'):
-				object_address = j[1].strip()
+				if object_address == '/list':
+					return self.list()
 				return self.http_get(object_address, all_headers)
-			if (method=='POST'):
-				object_address = j[1].strip()
+
+			elif (method=='POST'):
+				if object_address == '/upload':
+					body_index = data.find('\r\n\r\n') + 4
+					body = data[body_index:]
+					return self.upload(all_headers, body)
 				return self.http_post(object_address, all_headers)
+
+			elif (method == 'DELETE'):
+				if object_address.startswith('/delete'):
+					return self.delete(object_address)
+				return self.response(404, 'Not Found', '', {})
+   
 			else:
 				return self.response(400,'Bad Request','',{})
+
 		except IndexError:
 			return self.response(400,'Bad Request','',{})
+
 	def http_get(self,object_address,headers):
 		files = glob('./*')
 		#print(files)
@@ -106,7 +156,6 @@ if __name__=="__main__":
 	#print(d)
 #	d = httpserver.http_get('testing.txt')
 #	print(d)
-
 
 
 
