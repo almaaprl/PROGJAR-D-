84866fc4b76fc839a8538bae780fcb9d1dba3dc3
almaaprl <162028579+almaaprl@users.noreply.github.com>
2025-06-25 22:36:58 +0700

    Update server_thread_pool_http.py

--- Tugas 4/server_thread_pool_http.py
+++ Tugas 4/server_thread_pool_http.py
@@ -4,43 +4,54 @@ import time
 import sys
 import logging
 import multiprocessing
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 from http import HttpServer
 
 httpserver = HttpServer()
 
-#untuk menggunakan threadpool executor, karena tidak mendukung subclassing pada process,
+#untuk menggunakan processpoolexecutor, karena tidak mendukung subclassing pada process,
 #maka class ProcessTheClient dirubah dulu menjadi function, tanpda memodifikasi behaviour didalamnya
 
 def ProcessTheClient(connection,address):
 		rcv=""
 		while True:
 			try:
-				data = connection.recv(32)
-				if data:
-					#merubah input dari socket (berupa bytes) ke dalam string
-					#agar bisa mendeteksi \r\n
-					d = data.decode()
-					rcv=rcv+d
-					if rcv[-2:]=='\r\n':
-						#end of command, proses string
-						#logging.warning("data dari client: {}" . format(rcv))
-						hasil = httpserver.proses(rcv)
-						#hasil akan berupa bytes
-						#untuk bisa ditambahi dengan string, maka string harus di encode
-						hasil=hasil+"\r\n\r\n".encode()
-						#logging.warning("balas ke  client: {}" . format(hasil))
-						#hasil sudah dalam bentuk bytes
-						connection.sendall(hasil)
-						rcv=""
-						connection.close()
-						return
-				else:
+				data = connection.recv(1024)
+				if not data:
 					break
-			except OSError as e:
-				pass
+				d = data.decode()
+				rcv=rcv+d
+				if '\r\n\r\n' in rcv:
+						break
+			except OSError:
+				break
+
+		try:
+			header_part, body_part = rcv.split('\r\n\r\n', 1)
+		except ValueError:
+			connection.close()
+			return
+
+		headers = header_part.split('\r\n')
+		content_length = 0
+		for line in headers:
+			if line.lower().startswith("content-length:"):
+				content_length = int(line.split(":")[1].strip())
+
+		while len(body_part.encode()) < content_length:
+			try:
+				data = connection.recv(1024)
+				if not data:
+					break
+				body_part += data.decode()
+			except OSError:
+				break
+
+		full_request = header_part + '\r\n\r\n' + body_part
+		hasil = httpserver.proses(full_request)
+		hasil = hasil + b"\r\n\r\n"
+		connection.sendall(hasil)
 		connection.close()
-		return
 
 
 
@@ -49,10 +60,10 @@ def Server():
 	my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-	my_socket.bind(('0.0.0.0', 8885))
+	my_socket.bind(('0.0.0.0', 8889))
 	my_socket.listen(1)
 
-	with ThreadPoolExecutor(20) as executor:
+	with ProcessPoolExecutor(20) as executor:
 		while True:
 				connection, client_address = my_socket.accept()
 				#logging.warning("connection from {}".format(client_address))
@@ -63,12 +74,8 @@ def Server():
 				print(jumlah)
 
 
-
-
-
 def main():
 	Server()
 
 if __name__=="__main__":
 	main()
-
